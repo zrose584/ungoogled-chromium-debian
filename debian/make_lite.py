248a4013bdd49a5d75efe7b2efd2d0b4f76b2834
zrose584 <57181548+zrose584@users.noreply.github.com>
2019-11-09 21:37:47 +0100

    edit/add/remove some patches
    - fixes/closure.patch  is not needed
    - ungoogled-chromium/manpage.patch  debian's manpage got updated
    - ungoogled-chromium/disable-unrecognized-warnings.patch  for not cutting edge clang
    
    - make_lite_list.txt  files that should be deleted before building
    - make_lite.py  removes all files listed in make_lite_list.txt
    
    - debian/rules: disable some warnings and use make_lite.py

---
+++ debian/make_lite.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import shutil
+
+lines = open(sys.path[0] + '/make_lite_list.txt').read().splitlines()
+
+me = 'debian/' + os.path.basename(__file__)
+
+print("[{}] deleting {} unnecessary files/directories ...".format(me, len(lines)))
+
+n_files = 0
+n_dirs = 0
+
+for file in lines:
+    if file[0] == '#':
+        continue
+
+    abs_file = sys.path[0] + '/../' + file
+
+    if os.path.isfile(abs_file):
+        n_files += 1
+        os.remove(abs_file)
+    elif os.path.isdir(abs_file):
+        n_dirs += 1
+        shutil.rmtree(abs_file)
+    else:
+        print("[{}] no file: {}".format(me, file))
+
+print("[{}] deleted {} directories and {} files".format(me, n_dirs, n_files))
